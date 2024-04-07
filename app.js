034acdffd942ce87bcf1db03737634200fb8f044
Pintu Kumar <64580363+pintu544@users.noreply.github.com>
2024-04-07 12:36:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000; // Vercel will assign a port
 // MongoDB Connection
 mongoose
   .connect(
-    "mongodb+srv://pksharmagh4:0PggKBGRsE9uJfBz@cluster0.ffqsax5.mongodb.net/?retryWrites=true&w=majority" ||process.env.DATABASE_URL
+    "" ||process.env.DATABASE_URL
   )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error(err));
